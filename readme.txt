9979785c36b1d07ed7601961224b6e457dc93e1f
seldri <104066295+seldri@users.noreply.github.com>
2022-04-20 13:53:00 +0200

    Setup blueJ repo

---
+++ readme.txt
@@ -0,0 +1,2 @@
+This is the Branch for the Java BlueJ Training.
+It will have some stuff from different chapters starting with chapter 02.
---
+++ src/main/java/com/drini/Main.java
@@ -0,0 +1,10 @@
+package com.drini;
+import com.drini.chapter03.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Clockdisplay clock01 = new Clockdisplay(11, 23);
+        clock01.getDisplayValue();
+    }
+}
