7f28a50ad3c64cfb8899ac692f6b073baf11fe44
Drini <Selimaj>
2022-04-20 14:21:59 +0200

    Setup branch for chapter 02

--- src/main/java/com/blueJ/Main.java
+++ src/main/java/com/blueJ/Main.java
@@ -1,11 +1,8 @@
 package com.blueJ;
-//import com.blueJ.chapter03.*;
-import com.blueJ.chapter03.Clock.Clockdisplay;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        Clockdisplay clock01 = new Clockdisplay(11, 23);
-        System.out.println(clock01.getDisplayValue());
     }
 }
