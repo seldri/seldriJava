0b83bff9a7f8c554581b5772934940b07c326dee
Drini <Selimaj>
2022-05-04 14:52:08 +0200

    set variables to private

--- src/main/java/com/blueJ/chapter03/Clock/Clockdisplay.java
+++ src/main/java/com/blueJ/chapter03/Clock/Clockdisplay.java
@@ -6,9 +6,9 @@ public class Clockdisplay {
     /*Class to put together and display the time
     The variables of the class Clockdisplay hold two Objects from the type Numberdisplay
     One for hours and one for minutes*/
-    public Numberdisplay hours;
-    public Numberdisplay minutes;
-    public String displayValue;
+    private Numberdisplay hours;
+    private Numberdisplay minutes;
+    private String displayValue;
     //Constructor which sets hours and minutes of the clockdisplay to 0 and 0 by creating new Numberdisplay Objects
     public Clockdisplay(){
         hours = new Numberdisplay(24);
