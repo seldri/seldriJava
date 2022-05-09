9e1c26c6b49f780fcd67fc8a3d8b0ce2511a12d6
Drini <Selimaj>
2022-05-09 15:11:03 +0200

    Cleared chapter04 stuff to create new branch

--- src/main/java/com/blueJ/Main.java
+++ src/main/java/com/blueJ/Main.java
@@ -1,20 +1,12 @@
 package com.blueJ;
-import java.util.ArrayList;
 
 //import com.blueJ.chapter03.*;
 //import com.blueJ.chapter03.Clock.Clockdisplay;
 
 public class Main {
 
-    private static ArrayList<Integer> numbers;
-    
+
 
     public static void main(String[] args) {
-
-        numbers = new ArrayList<>();
-        numbers.add(4);
-        numbers.add(6);
-        System.out.println(numbers.get(1));
-
     }
 } 
