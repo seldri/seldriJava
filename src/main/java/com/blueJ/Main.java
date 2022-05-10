540adda3749131a708d3563eb0f73f4b002b8096
Drini <Selimaj>
2022-05-10 18:07:55 +0200

    continued working on ch04

--- src/main/java/com/blueJ/Main.java
+++ src/main/java/com/blueJ/Main.java
@@ -1,20 +1,66 @@
 package com.blueJ;
-import com.blueJ.chapter04.FirstPart.*;
-import com.blueJ.*;
+import com.blueJ.chapter04.MusicOrganizer;
+import com.blueJ.chapter04.ArrayListTraining;
 
 //import com.blueJ.chapter03.*;
 //import com.blueJ.chapter03.Clock.Clockdisplay;
 
+    
+
+    
 public class Main {
+
+    public static void calculate(int a, int b){
+
+        int sum = 0;
+        while(a < b - 1){
+            a++;
+            sum = sum + a;
+        }
+        System.out.println(sum);
+    }
+
+    public static void calculate2(int a, int b){
+
+        int sum = 0;
+        while(a <= b){
+            sum = sum + a;
+            a++;
+        }
+        System.out.println(sum);
+    }
+
+
+    public static boolean isPrime(int n){
+        int a = 2;
+        while(a < n){
+            int c = n % a;
+            if(c == 0){
+                return false;
+            }
+            a ++;
+        }
+        return true;
+
+    }
 
 
     public static void main(String[] args) {
 
-        MusicOrganizer m1 = new MusicOrganizer();
+        /*MusicOrganizer m1 = new MusicOrganizer();
         m1.addFile("A");
         m1.addFile("B");
         m1.addFile("C");
         m1.addFile("D");
-        m1.printFilesAndIndex();
+        m1.printElements(); */
+
+        /*ArrayListTraining a1 = new ArrayListTraining();
+        a1.addToList();
+        a1.selectiveSearch2("e");*/
+
+        
+        System.out.println(isPrime(18));
+        
+        
     }
 } 
