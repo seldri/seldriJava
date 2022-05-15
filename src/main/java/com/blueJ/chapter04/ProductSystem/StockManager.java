97ed5bb6162da2377c40deef0f1d1560a68d834d
Drini <Selimaj>
2022-05-15 23:46:46 +0200

    Last correction

--- src/main/java/com/blueJ/chapter04/ProductSystem/StockManager.java
+++ src/main/java/com/blueJ/chapter04/ProductSystem/StockManager.java
@@ -27,8 +27,8 @@ public class StockManager{
      * @param id The ID of the product.
      * @param amount The amount to increase the quantity by.
      */
-    //public delivery(int id, int amount){
-    //}
+    public delivery(int id, int amount){
+    }
 
     /**
      * Try to find a product in the stock with the given id.
