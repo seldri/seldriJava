f98c58f0db684e3b64eb26496bd13f72432c4f91
Drini <Selimaj>
2022-06-01 16:19:10 +0200

    Setup chapter06

--- src/main/java/com/blueJ/Main.java
+++ src/main/java/com/blueJ/Main.java
@@ -1,15 +1,12 @@
 package com.blueJ;
 
-//import com.blueJ.chapter03.*;
-//import com.blueJ.chapter03.Clock.Clockdisplay;
-import com.blueJ.chapter05.animalMonitoringV1.*;
+
 
 public class Main {
 
     
     public static void main(String[] args) {
     
-        AnimalMonitor a1 = new AnimalMonitor();
-        a1.addSightings("sightings.csv");
+       
     }
 } 
---
+++ src/main/java/com/blueJ/chapter06/techSupport1/Responder.java
@@ -0,0 +1,18 @@
+package com.blueJ.chapter06.techSupport1;
+
+public class Responder {
+    
+    /**
+     * Constructor of Responder
+     */
+    public Responder(){
+    }
+
+    /**
+     * Generate a response
+     * @return A String that should be displayed as the response
+     */
+    public String generateResponse(){
+        return "That sound interesting. Tell me more...";
+    }
+}
