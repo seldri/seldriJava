8f6740ead2cf72f883772c653f34e74ccd449bea
Drini <Selimaj>
2022-06-06 10:09:05 +0200

    Updated generateResponse method

--- src/main/java/com/blueJ/chapter06/techSupport1/Responder.java
+++ src/main/java/com/blueJ/chapter06/techSupport1/Responder.java
@@ -8,60 +8,95 @@ import java.util.HashSet;
 public class Responder{
 
     private Random randomGenerator;
-    private ArrayList<String> responses;
-    private HashMap<String, String> responseList;
+    private ArrayList<String> defaultResponses;
+    private HashMap<String, String> responses;
     private String message;
-    private String defaultMessage;
+    private String lastDefault;
 
     public Responder(){
         randomGenerator = new Random();
-        responses = new ArrayList<>();
-        responseList = new HashMap<>();
+        defaultResponses = new ArrayList<>();
+        responses = new HashMap<>();
+        fillDefaultResponses();
         fillResponses();
-        fillResponseList();
     }
 
     /**
      * Get a random response out of the list.
-     * @return Random response out of the responses.
+     * @return Random response out of the defaultResponses.
      */
     public String generateResponse(HashSet<String> input){
         
         for(String word : input){
-            String answer = responseList.get(word);
+            String answer = responses.get(word);
             if(word != null){
                 return answer;
             }
         }
-        message = responses.get(randomGenerator.nextInt(responses.size()));
-        if(!message.equals(defaultMessage)){
-            defaultMessage = message;
+        message = defaultResponses.get(randomGenerator.nextInt(defaultResponses.size()));
+        if(!message.equals(lastDefault)){
+            lastDefault = message;
             return message;
         }
         return generateResponse(input);
     }
 
-    /**
-     * Fill the list of responses.
-     */
-    public void fillResponses(){
-        responses.add("This sounds weird. Can you describe this more precise?");
-        responses.add("Until now no customer has complained about this. \n " + "Which system configuration do you have?");
-        responses.add("I need more precise answers.");
-        responses.add("Did you check if there is a conflict with DLL?");
-        responses.add("This is described in the manual. Did you read the manual?");
-        responses.add("This sounds too imprecise. Do you have an Expert \n " + "Which is near you?");
-        responses.add("This is no mistake, this is a system feature!");
-        responses.add("Could you describe it differently?");
-        responses.add("Did you try to use the app on your mobile phone?");
-        responses.add("I just checked on StackOverflow as well, there is also no solution.");
+    public String generateResponse3(HashSet<String> words){
+        int counter = 0;
+        //Create Array of matching keywords from user input
+        String[] matches = new String[words.size()];
+        String response = "";
+
+        for(String word : words){
+            matches[counter] = responses.get(word);
+            if(matches != null){
+                counter++;
+            }
+        }
+        
+        //Piece together a response, multiple pieces of advice, if multiple matches
+        for(counter = 0; counter < matches.length; counter++){
+            if(matches[counter] != null){
+                response = response + matches[counter] + "\n";
+            }
+        }
+        
+        if(response.length() > 1)
+            return response;
+            
+        //If no match found, provide default response
+        message = defaultResponses.get(randomGenerator.nextInt(defaultResponses.size()));
+        if(!message.equals(lastDefault)){
+            lastDefault = message;
+            return message;
+        }
+        
+        return generateResponse3(words);
     }
 
-    public void fillResponseList(){
-        responseList.put("froze", "End the system with your TaskManager and restart it again.");
-        responseList.put("slow", "This is a hardware problem, you should update your CPU.");
-        responseList.put("error", "The connection failed. You must be connected to the WiFi.");
-        responseList.put("battery low", "Your battery is low. You must recharge your device.");
-        responseList.put("old", "Your version is old. You must update your device.");
+    /**
+     * Fill the list of defaultResponses.
+     */
+    public void fillDefaultResponses(){
+        defaultResponses.add("This sounds weird. Can you describe this more precise?");
+        defaultResponses.add("Until now no customer has complained about this. \n " + "Which system configuration do you have?");
+        defaultResponses.add("I need more precise answers.");
+        defaultResponses.add("Did you check if there is a conflict with DLL?");
+        defaultResponses.add("This is described in the manual. Did you read the manual?");
+        defaultResponses.add("This sounds too imprecise. Do you have an Expert \n " + "Which is near you?");
+        defaultResponses.add("This is no mistake, this is a system feature!");
+        defaultResponses.add("Could you describe it differently?");
+        defaultResponses.add("Did you try to use the app on your mobile phone?");
+        defaultResponses.add("I just checked on StackOverflow as well, there is also no solution.");
+    }
+
+    public void fillResponses(){
+        responses.put("froze", "End the system with your TaskManager and restart it again.");
+        responses.put("slow", "This is a hardware problem, you should update your CPU.");
+        responses.put("error", "The connection failed. You must be connected to the WiFi.");
+        responses.put("battery low", "Your battery is low. You must recharge your device.");
+        responses.put("battery", "Your battery is low. You must recharge your device.");
+        responses.put("low battery", "Your battery is low. You must recharge your device.");
+        responses.put("old", "Your version is old. You must update your device.");
     }
 }
