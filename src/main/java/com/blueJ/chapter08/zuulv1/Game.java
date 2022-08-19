d6ef834bab938dfe2558b3b1157f6f85fb9da30a
Drini <Selimaj>
2022-08-19 11:22:58 +0200

    Finished setting up

--- src/main/java/com/blueJ/chapter08/zuulv1/Game.java
+++ src/main/java/com/blueJ/chapter08/zuulv1/Game.java
@@ -72,6 +72,92 @@ public class Game{
         System.out.println();
     }
 
-    private boolean processCommand(Command command){}
+    private boolean processCommand(Command command){
+        boolean wantToQuit = false;
 
+        if(command.isUnknown()){
+            System.out.println("I don't know what you mean...");
+            return false;
+        }
+
+        String commandWord = command.getCommandWord();
+        if(commandWord.equals("help")){
+            printHelp();
+        }
+        else if(commandWord.equals("go")){
+            goRoom(command);
+        }
+        else if(commandWord.equals("quit")){
+            wantToQuit = quit(command);
+        }
+        return wantToQuit;
+    }
+    
+    private void printHelp() 
+    {
+        System.out.println("You are lost. You are alone. You wander");
+        System.out.println("around at the university.");
+        System.out.println();
+        System.out.println("Your command words are:");
+        System.out.println("   go quit help");
+    }
+
+    private void goRoom(Command command) 
+    {
+        if(!command.hasSecondWord()) {
+            // if there is no second word, we don't know where to go...
+            System.out.println("Go where?");
+            return;
+        }
+
+        String direction = command.getSecondWord();
+
+        // Try to leave current room.
+        Room nextRoom = null;
+        if(direction.equals("north")) {
+            nextRoom = currentRoom.northExit;
+        }
+        if(direction.equals("east")) {
+            nextRoom = currentRoom.eastExit;
+        }
+        if(direction.equals("south")) {
+            nextRoom = currentRoom.southExit;
+        }
+        if(direction.equals("west")) {
+            nextRoom = currentRoom.westExit;
+        }
+
+        if (nextRoom == null) {
+            System.out.println("There is no door!");
+        }
+        else {
+            currentRoom = nextRoom;
+            System.out.println("You are " + currentRoom.getDescription());
+            System.out.print("Exits: ");
+            if(currentRoom.northExit != null) {
+                System.out.print("north ");
+            }
+            if(currentRoom.eastExit != null) {
+                System.out.print("east ");
+            }
+            if(currentRoom.southExit != null) {
+                System.out.print("south ");
+            }
+            if(currentRoom.westExit != null) {
+                System.out.print("west ");
+            }
+            System.out.println();
+        }
+    }
+
+    private boolean quit(Command command) 
+    {
+        if(command.hasSecondWord()) {
+            System.out.println("Quit what?");
+            return false;
+        }
+        else {
+            return true;  // signal that we want to quit
+        }
+    }
 }
