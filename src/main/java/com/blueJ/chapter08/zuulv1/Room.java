69ca4deebcd78a8711ac5af911aea316e412c8d1
Drini <Selimaj>
2022-08-22 14:46:56 +0200

    Removed room2 class

--- src/main/java/com/blueJ/chapter08/zuulv1/Room.java
+++ src/main/java/com/blueJ/chapter08/zuulv1/Room.java
@@ -1,66 +1,42 @@
 package com.blueJ.chapter08.zuulv1;
+import java.util.HashMap;
 
 public class Room{
 
     private String description;
-    private Room northExit;
-    private Room eastExit;
-    private Room southExit;
-    private Room westExit;
+    private HashMap<String, Room> exits;
 
     public Room(String description){
         this.description = description;
+        exits = new HashMap<>();
     }
 
     public String getDescription(){
         return description;
     }
 
+    public void setExits(String direction, Room neighbor){
+        exits.put(direction, neighbor);
+    }
+
     public Room getExit(String direction){
-        if(direction.equals("north")){
-            return northExit;
-        }
-        if(direction.equals("east")){
-            return eastExit;
-        }
-        if(direction.equals("south")){
-            return southExit;
-        }
-        if(direction.equals("west")){
-            return westExit;
-        }
-        return null;
+        return exits.get(direction);
     }
 
-    public void setExits(Room north, Room east, Room south, Room west){
-        if(north != null){
-            northExit = north;
-        }
-        if(east != null){
-            eastExit = east;
-        }
-        if(south != null){
-            southExit = south;
-        }
-        if(west != null){
-            westExit = west;
-        }
-    }
-
-    public void getExitListString(){
-        System.out.println("******************");
-        if(northExit != null){
-            System.out.println("North: " + northExit);
-        }
-        if(westExit != null){
-            System.out.println("West: " + westExit);
-        }
-        if(southExit != null){
-            System.out.println("South" + southExit);
-        }
-        if(eastExit != null){
-            System.out.println("East: " + eastExit);
-        }
-        System.out.println("******************");
-    }
+    // public void getExitListString(){
+    //     System.out.println("******************");
+    //     if(northExit != null){
+    //         System.out.println("North: " + northExit);
+    //     }
+    //     if(westExit != null){
+    //         System.out.println("West: " + westExit);
+    //     }
+    //     if(southExit != null){
+    //         System.out.println("South" + southExit);
+    //     }
+    //     if(eastExit != null){
+    //         System.out.println("East: " + eastExit);
+    //     }
+    //     System.out.println("******************");
+    // }
 }
