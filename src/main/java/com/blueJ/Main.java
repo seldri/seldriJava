06098d320a037e71c500ea7a5f889761c878014f
Drini <Selimaj>
2022-09-05 11:37:30 +0200

    Finished chapter08

--- src/main/java/com/blueJ/Main.java
+++ src/main/java/com/blueJ/Main.java
@@ -1,16 +1,11 @@
 package com.blueJ;
 // import java.lang.System;
 
-import com.blueJ.chapter08.zuulWEnumsV2.Game;
+import com.blueJ.chapter08.zuulWEnumsV2.*;
 
+public class Main{
 
-//import com.blueJ.chapter07.LogAnalyzer;
-
-
-public class Main {
-
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Game g1 = new Game();
         g1.play();
