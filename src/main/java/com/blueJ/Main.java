03014a43ff814ed4914d62b5f5c9d71fbef566f7
Drini <Selimaj>
2022-09-08 14:39:57 +0200

    Tried some stuff out

--- src/main/java/com/blueJ/Main.java
+++ src/main/java/com/blueJ/Main.java
@@ -1,13 +1,7 @@
 package com.blueJ;
-// import java.lang.System;
-
-import com.blueJ.chapter08.zuulWEnumsV2.*;
 
 public class Main{
 
     public static void main(String[] args){
-
-        Game g1 = new Game();
-        g1.play();
     }
 }
