e99b427aee0815de58cc5f4b64b34181c9c607d3
seldri <104066295+seldri@users.noreply.github.com>
2022-11-09 10:26:21 +0100

    Add files via upload

---
+++ src/main/java/com/blueJ/chapter07/weblogAnalyzer/LoglineTokenizer.java
@@ -0,0 +1,35 @@
+package com.blueJ.chapter07.weblogAnalyzer;
+import java.util.Scanner;
+
+public class LoglineTokenizer
+{
+    /**
+     * Construct a LogLineAnalyzer
+     */
+    public LoglineTokenizer()
+    {
+    }
+
+    /**
+     * Tokenize a log line. Place the integer values from
+     * it into an array. The number of tokens on the line
+     * must be sufficient to fill the array.
+     *
+     * @param logline The line to be tokenized.
+     * @param dataLine Where to store the values.
+     */
+    public void tokenize(String logline, int[] dataLine)
+    {
+        try {
+            // Scan the logline for integers.
+            Scanner tokenizer = new Scanner(logline);
+            for(int i = 0; i < dataLine.length; i++) {
+                dataLine[i] = tokenizer.nextInt();
+            }
+        }
+        catch(java.util.NoSuchElementException e) {
+            System.out.println("Insufficient data items on log line: " + logline);
+            throw e;
+        }
+    }
+}
