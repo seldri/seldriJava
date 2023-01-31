8e46294f744c71a09559eaf07e2d086f0a75d4b9
Drini <Selimaj>
2023-01-31 19:38:47 +0100

    Continued ch13

--- src/main/java/com/blueJ/chapter13/imageviewer01/ImageViewer.java
+++ src/main/java/com/blueJ/chapter13/imageviewer01/ImageViewer.java
@@ -15,10 +15,35 @@ public class ImageViewer {
         frame = new JFrame("ImageViewer");
         Container contentPane = frame.getContentPane();
 
-        JLabel label = new JLabel("Put label here");
+        JLabel label = new JLabel("Ich bin ein Label ich kann text darstellen");
         contentPane.add(label);
+
+        // JButton button = new JButton("hello");
+        // contentPane.add(button);
+
+        makeMenu();
 
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void makeMenu(){
+        JMenuBar menueBar = new JMenuBar();
+        frame.setJMenuBar(menueBar);
+
+        JMenu dateiMenue = new JMenu("Datei");
+        menueBar.add(dateiMenue);
+
+        JMenuItem openDatei = new JMenuItem("Öffnen");
+        dateiMenue.add(openDatei);
+
+        JMenuItem closeDatei = new JMenuItem("Beenden");
+        dateiMenue.add(closeDatei);
+
+        JMenu hilfeMenue = new JMenu("Hilfe");
+        menueBar.add(hilfeMenue);
+
+        JMenuItem hilfeInfo = new JMenuItem("Info");
+        hilfeMenue.add(hilfeInfo);
     }
 }
